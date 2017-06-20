bc55825f602a1fa1d8c928c99b79fed2302ea91c
Mervat <m.karam@idco.sa>
2017-06-20 02:00:26 +0200

    fix issues

--- backend/views/projects/_form.php
+++ backend/views/projects/_form.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
+use yii\helpers\Url;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Projects */
@@ -19,8 +20,22 @@ use yii\bootstrap\ActiveForm;
     <?= $form->field($model, 'logo')->fileInput(['multiple' => true, 'accept' => 'image/*']) ?>
 
     <?php
+
+//    echo Yii::$app->request->baseUrl."<br/>" ;
+//    print_r(Yii::$app->request->baseUrl);
+//    echo "<br/>";
+    var_dump(Yii::$app->urlManagerBackend);
+    echo "<br/>";
+//    echo Yii::$app->request->getBaseUrl(true);
+
+//    var_dump(Url::base());
+    die();
+//    var_dump(Yii::$app->getUrlManager()->baseUrl);die();
+//    var_dump(Yii::$app->request->baseUrl);die();
+
     if (isset($model->logo) && !empty($model->logo))
-        echo Html::img(Yii::getAlias('@webroot') . '/uploads/' . $model->logo, [
+//        echo Html::img(Yii::getAlias('@webroot') . '/uploads/' . $model->logo, [
+        echo Html::img( 'http://backend.erp.com/uploads/' . $model->logo, [
             'width' => 100,
             'height' => 75
         ]);
